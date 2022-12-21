b6b60373361affad5a46bae95d809efde11f1897
Precious Ibeagi <preciousaang@gmail.com>
2022-12-21 20:57:24 +0100

    is it a good song

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>AGZ App is good for you</h1>
+      <h1>Viva la vida is a good song</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
